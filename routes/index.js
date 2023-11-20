d11051e93cb5d6f296496a3d9d5c60c2904d10d7
fahmitaris0901 <mohammad.syahrir@smartfren.com>
2023-11-20 13:03:51 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ const requestIP = require('request-ip');
 
 router.use("/api/digiflazz", async(req,res) => {
   try{
-    const ipAddress = requestIP.getClientIp(req);
+    const ipAddress = requestIP.getClientIp(req).split(',').shift();
     const reqData = {
       username: req.body.username,
       commands: req.body.commands,
